4c6e2925c8115deff1809f88984be300fb1f3123
Mikael Popowicz <mikael.popowicz@gmail.com>
2019-11-01 18:34:54 +0100

    :tada: Initial commit.

---
+++ src/PermissionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mikaelpopowicz\NovaPermission;
+
+use Illuminate\Support\ServiceProvider;
+
+class PermissionServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+
+    }
+
+    public function register()
+    {
+
+    }
+}
